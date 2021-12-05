c410351a534f8ee7300b4c70f8e8ad6ab04fdab8
lomekragow <ShieldPotocol@outlook.com>
2021-12-05 22:29:06 +0800

    太难了，终于把my-windows的错误修好了，新版本

--- ports/auto-tools/portfile.cmake
+++ ports/auto-tools/portfile.cmake
@@ -4,8 +4,8 @@ vcpkg_check_linkage(ONLY_DYNAMIC_CRT)   # 强制使用动态CRT
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Chaojimengnan/auto-tools
-    REF 1aacedd7520ae68aa8b0473edefa917cbefd9dcc # 0.0.2
-    SHA512 6c61960528e78ccfc0c12fa3ce98befb624ff3e24596b7cec382a64866bf179e2502da8029740065bf186b446d32acefd316092bc2e7551331475178f5e02bf8
+    REF badeecc0a82e74a29064b882a04bed7410396bca # 0.0.3
+    SHA512 524a6c56f97de59f9d7b814a63c0e0b2b68ce21c57536204a817bba209d776c536f62171df2bee97a0b3df6659ddfce0cdfa6f01807ff925a7f6cee92c0769f7
     HEAD_REF master
 )
 
