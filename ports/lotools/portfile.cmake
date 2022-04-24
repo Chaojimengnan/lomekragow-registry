08e8c7dfd24284efe234827e1a5117d7b77ed39e
lomekragow <shieldpotocol@outlook.com>
2022-04-24 15:38:58 +0800

    自动工具:更新port lotools

--- ports/lotools/portfile.cmake
+++ ports/lotools/portfile.cmake
@@ -4,8 +4,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Chaojimengnan/lotools
-    REF 79eb257fe5f03c96aa7139b4bedf52be7b04eeaf # 0.0.3
-    SHA512 de1b75823790dfee49a06f1c87c52dd366528914aa1bd545214ed9fdb0dfadc2c580835d1e31833bea1358a75d833d5eb67b628a51208518ec882413bac510c8
+    REF cc31e3f54e695bef02c4c28874e967ee8bf24e62 # 0.0.4
+    SHA512 c1abebfe4c75a41e9c70b933b917214e1084a1d10ab6919c2c4eede24f9e672bee36d197f63aa7e6c86b0776b500cc6c2d5ab0fed12ad2c5fa6d83d67ecae9d6
     HEAD_REF master
 )
 
